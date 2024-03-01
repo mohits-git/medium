eba5df02cedca0790ae245579f8fffd2a2dfa87c
mohits-git <mohitkhatri2805@gmail.com>
2024-03-01 18:27:48 +0530

    added api routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,28 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/user/signup', (c) => {
+    return c.text('Hello World!')
+})
+
+app.post('/api/v1/user/signin', (c) => {
+    return c.text('Hello World!')
+})
+
+app.post('/api/v1/blog', (c) => {
+    return c.text('Hello World!')
+})
+
+app.put('/api/v1/blog', (c) => {
+    return c.text('Hello World!')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+    return c.text('Hello World!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+    return c.text('Hello World!')
 })
 
 export default app

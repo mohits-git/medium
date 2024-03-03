c39050dd46b3e3eeeb382dea50d6525cc1f1d2fb
mohits-git <mohitkhatri2805@gmail.com>
2024-03-03 17:10:47 +0530

    fix: cors error fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { blogRouter, userRouter } from './routes';
+import { cors } from 'hono/cors';
 
 const app = new Hono<{
     Bindings: {
@@ -8,6 +9,7 @@ const app = new Hono<{
     }
 }>();
 
+app.use('/*', cors());
 app.route('/api/v1/user', userRouter);
 app.route('/api/v1/blog', blogRouter);
 
